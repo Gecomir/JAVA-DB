9e86f1bb9d2bc8bbb9debed87f1da1aad9b3bca8
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-09-17 12:10:23 +0300

    Delete InsertDataInTables.sql

--- Databases Introduction. Data Definition and Datatypes/InsertDataInTables.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `employees` (`first_name`, `last_name`) VALUES ('Test1', 'Test1');
-INSERT INTO `employees` (`first_name`, `last_name`) VALUES ('Test2', 'Test2');
-INSERT INTO `employees` (`first_name`, `last_name`) VALUES ('Test3', 'Test3');
