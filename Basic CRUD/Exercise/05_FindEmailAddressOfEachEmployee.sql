765b6d1a20dd02bbac58f4058430c887ce3e8809
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-09-20 22:08:35 +0300

    Delete 05_FindEmailAddressOfEachEmployee.sql

--- Basic CRUD/Exercise/05_FindEmailAddressOfEachEmployee.sql
+++
@@ -1,9 +0,0 @@
-USE `soft_uni`;
-# 05. Find Email Address of Each Employee
-# email: {first name} . {last name} @softuni.bg
-SELECT 
-    CONCAT(`first_name`,
-            '.',
-            `last_name`,
-            '@softuni.bg') AS `full_email_address`
-FROM `employees`;
