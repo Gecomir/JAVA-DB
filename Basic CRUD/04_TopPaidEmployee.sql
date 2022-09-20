02a07ff8fe6d5e7887bea35134fd49e97f235f5f
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-09-20 15:53:25 +0300

    Add files via upload

---
+++ Basic CRUD/04_TopPaidEmployee.sql
@@ -0,0 +1,17 @@
+USE `hotel`;
+SELECT 
+    *
+FROM
+    `employees`
+ORDER BY `salary` DESC
+LIMIT 1;
+
+CREATE VIEW `v_top_paid_employee` AS
+SELECT 
+    *
+FROM
+    `employees`
+ORDER BY `salary` DESC
+LIMIT 1;
+
+SELECT * FROM `v_top_paid_employee`;
