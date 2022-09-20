28ebe972549f33e647ffd28a1b0a4ab53e51d496
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-09-20 15:54:06 +0300

    Add files via upload

---
+++ Basic CRUD/06_DeleteFromTable.sql
@@ -0,0 +1,8 @@
+USE `hotel`;
+DELETE FROM `employees`
+WHERE `department_id` = 1 OR `department_id` = 2;
+-- WHERE `department_id` IN (1, 2);
+-- WHERE `department_id` BETWEEN 1 AND 2;
+
+SELECT * FROM `employees`
+ORDER BY `id`;
