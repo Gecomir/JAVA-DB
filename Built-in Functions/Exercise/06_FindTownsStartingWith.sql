aefd7409db37cb00d66f673a8ff3d2c4688177f0
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-09-24 17:36:41 +0300

    Delete 06_FindTownsStartingWith.sql

--- Built-in Functions/Exercise/06_FindTownsStartingWith.sql
+++
@@ -1,5 +0,0 @@
-# 6. Find Towns Starting With
-SELECT * FROM `towns`
-#WHERE `name` LIKE 'M%' OR `name` LIKE 'K%' OR `name` LIKE 'B%' OR `name` LIKE 'E%'
-WHERE `name` REGEXP '^[MKBE]'
-ORDER BY `name`;
