76a03b3ee488c37a1fd0dc44beaa7fd4595543a7
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-09-29 21:49:33 +0300

    Add files via upload

---
+++ Data Aggregation/Exercise/06_DepositsSumForOllivanderFamily.sql
@@ -0,0 +1,6 @@
+# 06. Deposits Sum for Ollivander Family
+SELECT `deposit_group`, SUM(`deposit_amount`) AS `total_sum`
+FROM `wizzard_deposits`
+WHERE `magic_wand_creator` LIKE 'Ollivander family'
+GROUP BY `deposit_group`
+ORDER BY `deposit_group` ASC;
