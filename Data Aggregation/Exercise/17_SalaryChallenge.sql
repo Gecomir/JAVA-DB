a3ad836c0b7329d7ffd1879cbdb9a16b68cff981
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-10-01 13:56:40 +0300

    Add files via upload

---
+++ Data Aggregation/Exercise/17_SalaryChallenge.sql
@@ -0,0 +1,8 @@
+# 17. Salary Challenge
+SELECT `first_name`, `last_name`, `department_id` FROM `employees` current_employee
+WHERE `salary` > (
+				SELECT AVG(`salary`) FROM `employees` other_employee
+				WHERE current_employee.`department_id` = other_employee.`department_id`)
+ORDER BY `department_id`, `employee_id`
+LIMIT 10;
+
