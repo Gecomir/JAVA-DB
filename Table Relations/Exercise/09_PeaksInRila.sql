fe530fbf7e784fba573f094e82ff6f21d517f9f8
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-10-02 16:39:03 +0300

    Add files via upload

---
+++ Table Relations/Exercise/09_PeaksInRila.sql
@@ -0,0 +1,10 @@
+# 09. Peaks in Rila
+SELECT m.`mountain_range`, p.`peak_name`, p.`elevation`
+FROM mountains AS m
+JOIN peaks AS p
+ON m.`id` = p.`mountain_id`
+WHERE m.`mountain_range` LIKE 'Rila'
+ORDER BY p.`elevation` DESC;
+ 
+ 
+
