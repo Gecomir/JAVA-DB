cd6db914de013a611b0da61fa560cc7b8b987a5d
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-10-02 16:39:20 +0300

    Add files via upload

---
+++ Table Relations/Exercise/01-06-09.sql
@@ -0,0 +1,242 @@
+# 01. One-To-One Relationship
+CREATE TABLE `passports` (
+`passport_id` INT PRIMARY KEY AUTO_INCREMENT,
+`passport_number` VARCHAR(50) UNIQUE
+);
+
+INSERT INTO `passports` (`passport_id`, `passport_number`)
+VALUES (101, 'N34FG21B'),
+ (102, 'K65LO4R7'),
+ (103, 'ZE657QP2');
+ 
+ CREATE TABLE `people` (
+`person_id` INT PRIMARY KEY AUTO_INCREMENT,
+`first_name` VARCHAR(50) NOT NULL,
+`salary` DECIMAL(7,2),
+`passport_id` INT UNIQUE,
+CONSTRAINT fk_people_passports
+FOREIGN KEY (`passport_id`)
+REFERENCES `passports` (`passport_id`)
+);
+
+INSERT INTO `people` (`first_name`, `salary`, `passport_id`)
+VALUES('Roberto', 43300.00, 102),
+('Tom', 56100.00, 103),
+('Yana', 60200.00, 101);
+
+
+# 02. One-To-Many Relationship
+CREATE TABLE `manufacturers` (
+   `manufacturer_id` INT PRIMARY KEY AUTO_INCREMENT,
+   `name` VARCHAR(50) NOT NULL UNIQUE,
+   `established_on` DATE
+   );
+  
+INSERT INTO manufacturers ( `name`, `established_on`) 
+VALUES 
+	('BMW', '1916-03-01'), 
+	('Tesla', '2003-01-01'), 
+	('Lada', '1966-05-01');
+
+CREATE TABLE `models` (
+`model_id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50) NOT NULL UNIQUE,
+`manufacturer_id` INT,
+CONSTRAINT fk_models_manufacturers
+FOREIGN KEY (`manufacturer_id`)
+REFERENCES manufacturers (`manufacturer_id`)
+);
+
+INSERT INTO models (`model_id`, `name`, `manufacturer_id`) 
+VALUES 
+	(101, 'X1', 1),
+	(102, 'i6', 1),
+	(103, 'Model S', 2),
+	(104, 'Model X', 2),
+	(105, 'Model 3', 2),
+	(106, 'Nova', 3);
+    
+    
+# 03. Many-To-Many Relationship
+CREATE TABLE `students` (
+   `student_id` INT PRIMARY KEY AUTO_INCREMENT,
+   `name` VARCHAR(45) NOT NULL
+   );
+  
+INSERT INTO students (`name`) 
+VALUES 
+	('Mila'), 
+	('Toni'), 
+	('Ron');
+
+CREATE TABLE `exams` (
+   `exam_id` INT PRIMARY KEY AUTO_INCREMENT,
+   `name` VARCHAR(50) NOT NULL
+   );
+  
+INSERT INTO exams (`exam_id`, `name`) 
+VALUES 
+	(101, 'Spring MVC'), 
+	(102, 'Neo4j'), 
+	(103, 'Oracle 11g');
+    
+CREATE TABLE `students_exams` (
+`student_id` INT NOT NULL, 
+`exam_id` INT NOT NULL,
+CONSTRAINT pk
+PRIMARY KEY (`student_id`, `exam_id`),
+
+CONSTRAINT fk_students_exams_exams
+FOREIGN KEY (`exam_id`)
+REFERENCES `exams`(`exam_id`),
+
+CONSTRAINT fk_students_exams_students
+FOREIGN KEY (`student_id`)
+REFERENCES `students`(`student_id`)
+);
+
+INSERT INTO students_exams (`student_id`, `exam_id`) 
+VALUES 
+	(1,	101),
+	(1,	102),
+	(2,	101),
+	(3,	103),
+	(2,	102),
+	(2,	103);
+    
+    
+# 04. Self-Referencing
+CREATE TABLE `teachers`(
+`teacher_id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(50) NOT NULL,
+`manager_id` INT
+);
+
+ALTER TABLE `teachers` AUTO_INCREMENT=101;
+
+INSERT INTO teachers (`name`, `manager_id`) 
+VALUES 
+	('John', NULL),		
+	('Maya', 106),
+	('Silvia', 106),
+	('Ted', 105),
+	('Mark', 101),
+	('Greta', 101);
+
+ALTER TABLE `teachers`
+ADD CONSTRAINT pk_teacher
+FOREIGN KEY (`manager_id`)
+REFERENCES `teachers` (`teacher_id`);
+
+# 05. Online Store Database
+CREATE TABLE `cities` (
+	`city_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `item_types` (
+	`item_type_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `customers` (
+	`customer_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `birthday` DATE,
+    `city_id` INT NULL,
+    CONSTRAINT fk_customer_city
+    FOREIGN KEY (`city_id`)
+    REFERENCES `cities` (`city_id`)
+);
+
+CREATE TABLE `orders` (
+	`order_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `customer_id` INT NOT NULL,
+    CONSTRAINT fk_order_customer
+    FOREIGN KEY (`customer_id`)
+    REFERENCES `customers` (`customer_id`)
+);
+
+CREATE TABLE `items` (
+	`item_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `item_type_id` INT NOT NULL,
+    CONSTRAINT fk_items_type
+    FOREIGN KEY (`item_type_id`)
+    REFERENCES `item_types` (`item_type_id`)
+);
+
+CREATE TABLE `order_items` (
+	`order_id` INT NOT NULL,
+    `item_id` INT NOT NULL,
+    CONSTRAINT pk
+    PRIMARY KEY(order_id, item_id),
+    CONSTRAINT fk_order
+    FOREIGN KEY (`order_id`)
+    REFERENCES `orders` (`order_id`), 
+    CONSTRAINT fk_item
+    FOREIGN KEY (`item_id`)
+    REFERENCES `items` (`item_id`)
+);
+
+# 06. University Database
+CREATE TABLE `subjects` (
+	`subject_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `subject_name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `majors` (
+	`major_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `payments`(
+`payment_id` INT PRIMARY KEY AUTO_INCREMENT,
+`payment_date` DATE,
+`payment_amount` DECIMAL(8,2),
+`student_id` INT
+ );
+ 
+ CREATE TABLE `students`(
+ `student_id` INT PRIMARY KEY AUTO_INCREMENT,
+ `student_number` VARCHAR(12),
+ `student_name` VARCHAR(50),
+ `major_id` INT
+ );
+ 
+ CREATE TABLE `agenda`(
+ `student_id` INT,
+ `subject_id` INT,
+ 
+ CONSTRAINT pk
+ PRIMARY KEY(`student_id`,`subject_id`)
+ );
+ 
+ ALTER TABLE `agenda`
+ ADD CONSTRAINT fk_subjects
+ FOREIGN KEY(`subject_id`)
+ REFERENCES `subjects` (`subject_id`);
+ 
+ALTER TABLE `agenda`
+ ADD CONSTRAINT fk_students
+ FOREIGN KEY(`student_id`)
+ REFERENCES `students` (`student_id`);
+ 
+ ALTER TABLE `students`
+ ADD CONSTRAINT fk_majors
+ FOREIGN KEY (`major_id`)
+ REFERENCES `majors` (`major_id`);
+ 
+  ALTER TABLE `payments`
+ ADD CONSTRAINT fk_payments_students
+ FOREIGN KEY (`student_id`)
+ REFERENCES `students`(`student_id`);
+
+
+# 09. Peaks in Rila
+SELECT m.`mountain_range`, p.`peak_name`, p.`elevation`
+FROM mountains AS m
+JOIN peaks AS p
+ON m.`id` = p.`mountain_id`
+WHERE m.`mountain_range` LIKE 'Rila'
+ORDER BY p.`elevation` DESC;
