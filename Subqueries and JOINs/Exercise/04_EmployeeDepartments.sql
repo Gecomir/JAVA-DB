07bb9b789cdf07605686191fbbe8b9c9a7b0e374
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-10-06 23:46:32 +0300

    Delete 04_EmployeeDepartments.sql

--- Subqueries and JOINs/Exercise/04_EmployeeDepartments.sql
+++
@@ -1,8 +0,0 @@
-# 04. Employee Departments
-SELECT e.`employee_id`, e.`first_name`, e.`salary`, d.`name` AS `department_name`
-FROM `employees` AS e
-RIGHT JOIN `departments` AS d
-ON e.`department_id` = d.`department_id`
-WHERE e.`salary` > 15000
-ORDER BY d.`department_id` DESC
-LIMIT 5;
