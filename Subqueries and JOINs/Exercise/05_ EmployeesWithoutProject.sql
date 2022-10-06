5196ce8b3859b8b08c597ceceac9b48ff5b57211
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-10-06 23:44:05 +0300

    Add files via upload

---
+++ Subqueries and JOINs/Exercise/05_ EmployeesWithoutProject.sql
@@ -0,0 +1,8 @@
+# 05. Employees Without Project
+SELECT e.`employee_id`, e.`first_name`
+FROM `employees` AS e
+LEFT JOIN `employees_projects` AS p
+ON e.`employee_id` = p.`employee_id`
+WHERE p.`project_id` IS NULL
+ORDER BY `employee_id` DESC
+LIMIT 3;
