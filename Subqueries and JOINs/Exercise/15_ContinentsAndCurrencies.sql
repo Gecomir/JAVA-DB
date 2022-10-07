015e8f60ebaed2b34a9d9b83fa2135f0828aa068
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-10-08 01:52:42 +0300

    Add files via upload

---
+++ Subqueries and JOINs/Exercise/15_ContinentsAndCurrencies.sql
@@ -0,0 +1,13 @@
+# 15. *Continents and Currencies
+SELECT `continent_code`, `currency_code`, COUNT(`currency_code`) AS `currency_usage`
+FROM `countries` c1
+GROUP BY `continent_code`, `currency_code`
+HAVING `currency_usage` = (
+  SELECT COUNT(`currency_code`) AS `count`
+    FROM `countries` c2
+    WHERE c2.`continent_code` = c1.`continent_code`
+    GROUP BY c2.`currency_code`
+    ORDER BY `count` DESC
+    LIMIT 1
+  ) AND `currency_usage` > 1
+ORDER BY `continent_code`, `currency_code`;
