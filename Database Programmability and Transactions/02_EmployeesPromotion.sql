be98ce63ed3a6bd8328fedaad675546543434f78
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-10-08 17:14:23 +0300

    Add files via upload

---
+++ Database Programmability and Transactions/02_EmployeesPromotion.sql
@@ -0,0 +1,10 @@
+# 2. Employees Promotion
+DELIMITER $$
+CREATE PROCEDURE usp_raise_salaries(`department_name` VARCHAR(50))
+BEGIN
+	UPDATE `employees` AS e
+	JOIN `departments` AS d ON d.`department_id` = e.`department_id`
+    SET `salary` = `salary` * 1.05
+    WHERE d.`name` = `department_name`;
+END$$
+CALL `usp_raise_salaries`('Finance');
