c4bff39ae265d1fe682b332457744814ae4cb2c6
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-10-10 23:46:51 +0300

    Add files via upload

---
+++ Database Programmability and Transactions/Exercise/07_DefineFunction.sql
@@ -0,0 +1,8 @@
+# 07. Define Function
+DELIMITER ##
+CREATE FUNCTION ufn_is_word_comprised(set_of_letters VARCHAR(50), word VARCHAR(50))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+	RETURN word REGEXP (CONCAT('^[', set_of_letters, ']+$'));
+END##
