0df5d08f8d03b9af57049e10eba236f148b1d117
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-10-23 15:56:36 +0300

    Add files via upload

---
+++ SPRING DATA/DB Apps Introduction/Exercise/IncreaseAgeStoredProcedure.java
@@ -0,0 +1,58 @@
+public class IncreaseAgeStoredProcedure {
+    private static Connection connection;
+    private static int minionId;
+    public static void main(String[] args) throws SQLException {
+
+        minionId = getMinionId();
+
+        connection = E00Connection.getConnection();
+
+        ResultSet minionInfo = getMinionInfo();
+
+        if(minionInfo.next()) {
+            updateMinionAge();
+
+            minionInfo = getMinionInfo();
+
+            printMinionInfo(minionInfo);
+
+        } else {
+            System.out.printf(E01_Outputs.E09_PRINT_NO_MINION_WHIT_ID, minionId);
+        }
+
+        connection.close();
+    }
+
+    private static void printMinionInfo(ResultSet rs) throws SQLException {
+        rs.next();
+
+        String name = rs.getString("name");
+        int age = rs.getInt("age");
+
+        System.out.println( name + " " + age);
+    }
+
+    private static ResultSet getMinionInfo() throws SQLException {
+        PreparedStatement selectMinion = connection.prepareStatement(E01_Statement.E09_SELECT_MINIONS_INFO_BY_ID_SQL);
+
+        selectMinion.setInt(1, minionId);
+
+        return selectMinion.executeQuery();
+    }
+
+    private static void updateMinionAge() throws SQLException {
+        CallableStatement updateMinionAge = connection.prepareCall(E01_Statement.E09_CALL_PROCEDURE);
+
+        updateMinionAge.setInt(1, minionId);
+
+        updateMinionAge.executeUpdate();
+    }
+
+    public static int getMinionId() {
+        Scanner console = new Scanner(System.in);
+
+        System.out.print(E01_Outputs.E03_ENTER_ID);
+
+        return Integer.parseInt(console.nextLine());
+
+    }
