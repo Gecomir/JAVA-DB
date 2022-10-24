bda1d8c1d588df5d4ab9aced3edb9e0058a95a34
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-10-24 22:41:51 +0300

    Add files via upload

---
+++ SPRING DATA/DB Apps Introduction/Exercise/IncreaseAgeStoredProcedure.java
@@ -0,0 +1,35 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class IncreaseAgeStoredProcedure {
+    private static final String CALL_PROCEDURE = "CALL usp_get_older(?)";
+    private static final String MINIONS_INFO = "SELECT name, age FROM minions WHERE id = ?";
+    private static final String PRINT_MINION = "%s %d%n";
+
+    public static void main(String[] args) throws SQLException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int minionId = Integer.parseInt(scanner.nextLine());
+        Connection connection = Utils.getConnection();
+
+        CallableStatement callableStatement = connection.prepareCall(CALL_PROCEDURE);
+        callableStatement.setInt(1, minionId);
+        callableStatement.executeUpdate();
+
+        try {
+            PreparedStatement minionInfo = connection.prepareStatement(MINIONS_INFO);
+            ResultSet minionSet = minionInfo.executeQuery();
+
+            minionSet.next();
+            String minionName = minionSet.getString("name");
+            int minionAge = minionSet.getInt("age");
+
+            System.out.printf(PRINT_MINION, minionName, minionAge);
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        connection.close();
+    }
+}
