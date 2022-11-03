9c2d34f330a209c500684d11eb867314aeca3850
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-11-03 12:21:00 +0200

    Add files via upload

---
+++ SPRING DATA/Introduction to Hibernate/Exercise/src/main/java/entities/IncreaseSalaries.java
@@ -0,0 +1,38 @@
+package entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+public class IncreaseSalaries {
+    public static void main(String[] args) {
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("soft_uni");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        List<String> departments = Arrays.asList("Engineering", "Tool Design", "Marketing", "Information Services");
+
+        entityManager.createQuery("SELECT e FROM Employee e " +
+                        " WHERE e.department.name IN ( :departments ) ", Employee.class).
+                setParameter("departments", departments)
+                .getResultList()
+                .forEach(e -> {
+                    e.setSalary(e.getSalary().multiply(BigDecimal.valueOf(1.12)));
+                    entityManager.persist(e);
+                });
+
+        entityManager.createQuery("SELECT e FROM Employee e " +
+                        " WHERE e.department.name IN ( :departments ) ", Employee.class).
+                setParameter("departments", departments).
+                getResultList()
+                .forEach(e -> System.out.printf("%s %s ($%.2f)%n", e.getFirstName(), e.getLastName(), e.getSalary()));
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+    }
+}
