878e30b5489d20aa4bcb6d80c43ed044ce99dfe4
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-11-06 22:20:29 +0200

    Add files via upload

---
+++ SPRING DATA/Hibernate Code First/Exercise/src/main/java/entities/Main.java
@@ -0,0 +1,27 @@
+package entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("database");
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        WizardDeposits wizardDeposits = new WizardDeposits("George", "Ivanov", "test", 30, "Gecomir",
+                (short) 25, "all", LocalDateTime.now(), BigDecimal.valueOf(7777.77), BigDecimal.valueOf(7.5),
+                BigDecimal.valueOf(0.25), LocalDateTime.of(2022, 11, 1, 14, 41), false);
+
+        entityManager.persist(wizardDeposits);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+}
+
