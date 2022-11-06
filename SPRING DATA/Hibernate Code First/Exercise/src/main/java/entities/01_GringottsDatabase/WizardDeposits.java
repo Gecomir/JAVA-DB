a1582303ebace671ad9f783a0b34e6328649e57f
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-11-06 22:23:03 +0200

    Add files via upload

---
+++ SPRING DATA/Hibernate Code First/Exercise/src/main/java/entities/01_GringottsDatabase/WizardDeposits.java
@@ -0,0 +1,82 @@
+package entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "wizard_deposits")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public class WizardDeposits {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name", length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", length = 60, nullable = false)
+    private String lastName;
+
+    @Column(length = 1000)
+    private String notes;
+
+    @Column(name = "age", nullable = false)
+    private int age;
+
+    @Column(name = "magic_wand_creator", length = 100)
+    private String magicWandCreator;
+
+    @Column(name = "magic_wand_size")
+    private short magicWandSize;
+
+    @Column(name = "deposit_group", length = 20)
+    private String depositGroup;
+
+    @Column(name = "deposit_start_date", nullable = false)
+    private LocalDateTime depositStartDate;
+
+    @Column(name = "deposit_amount", nullable = false)
+    private BigDecimal depositAmount;
+
+    @Column(name = "deposit_interest", nullable = false)
+    private BigDecimal depositInterest;
+
+    @Column(name = "deposit_charge", nullable = false)
+    private BigDecimal depositCharge;
+
+    @Column(name = "deposit_expiration_date")
+    private LocalDateTime depositExpirationDate;
+
+    @Column(name = "is_deposit_expired")
+    private boolean isDepositExpired;
+
+    public WizardDeposits(String firstName, String lastName, String notes, int age,
+                          String magicWandCreator, short magicWandSize, String depositGroup,
+                          LocalDateTime depositStartDate, BigDecimal depositAmount,
+                          BigDecimal depositInterest, BigDecimal depositCharge,
+                          LocalDateTime depositExpirationDate, boolean isDepositExpired) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.notes = notes;
+        this.age = age;
+        this.magicWandCreator = magicWandCreator;
+        this.magicWandSize = magicWandSize;
+        this.depositGroup = depositGroup;
+        this.depositStartDate = depositStartDate;
+        this.depositAmount = depositAmount;
+        this.depositInterest = depositInterest;
+        this.depositCharge = depositCharge;
+        this.depositExpirationDate = depositExpirationDate;
+        this.isDepositExpired = isDepositExpired;
+    }
+
+}
