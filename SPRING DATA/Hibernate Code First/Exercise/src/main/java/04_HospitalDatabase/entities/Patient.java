fa2b0d8251c47156a3417f3b0bddf4704389fffa
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-11-08 22:37:28 +0200

    Add files via upload

---
+++ SPRING DATA/Hibernate Code First/Exercise/src/main/java/04_HospitalDatabase/entities/Patient.java
@@ -0,0 +1,124 @@
+package _04_HospitalDatabase.entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity(name = "_04_patients")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column
+    private String address;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(name = "date_of_birth", nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Column
+    private byte[] picture;
+
+    @Column(name = "has_medical_insurance", nullable = false)
+    private boolean isInsured;
+
+    @OneToMany(mappedBy = "patient")
+    private Set<Visitation> visitations;
+
+    @ManyToMany
+    @JoinTable(name = "diagnose_patients", joinColumns = @JoinColumn(name = "patient_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "diagnose_id", referencedColumnName = "id"))
+    private List<Diagnose> diagnoses;
+
+    public Patient() {
+        super();
+    }
+
+    public Patient(String firstName, String lastName, String address, String email, LocalDate dateOfBirth, boolean isInsured) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.isInsured = isInsured;
+        this.visitations = new HashSet<>();
+        this.diagnoses = new ArrayList<>();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public byte[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(byte[] picture) {
+        this.picture = picture;
+    }
+
+    public boolean isInsured() {
+        return isInsured;
+    }
+
+    public void setInsured(boolean insured) {
+        isInsured = insured;
+    }
+
+    public Set<Visitation> getVisitations() {
+        return visitations;
+    }
+
+    public void setVisitations(Set<Visitation> visitations) {
+        this.visitations = visitations;
+    }
+
+}
