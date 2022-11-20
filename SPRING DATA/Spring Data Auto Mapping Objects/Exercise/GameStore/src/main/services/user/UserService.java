f32ead0cd07d4425558149d9509a63ac5949f4c9
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-11-20 13:29:53 +0200

    Add files via upload

---
+++ SPRING DATA/Spring Data Auto Mapping Objects/Exercise/GameStore/src/main/services/user/UserService.java
@@ -0,0 +1,15 @@
+package com.softuni.gamestore.services.user;
+
+import com.softuni.gamestore.domain.entities.User;
+
+import java.util.Optional;
+
+public interface UserService {
+    String registerUser(String[] args);
+
+    String loginUser(String[] args);
+
+    String logoutUser();
+
+    User getLoggedInUser();
+}
