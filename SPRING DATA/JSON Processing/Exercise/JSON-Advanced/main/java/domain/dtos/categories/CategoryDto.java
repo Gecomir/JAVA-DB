32e8533c46f760d5abd2f9520eb37916fdee0b47
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-11-22 22:12:13 +0200

    Add files via upload

---
+++ SPRING DATA/JSON Processing/Exercise/JSON-Advanced/main/java/domain/dtos/categories/CategoryDto.java
@@ -0,0 +1,16 @@
+package com.example.jsonadvanced.domain.dtos.categories;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto {
+
+    private String name;
+
+}
