8094c8576a35a662dad0846d0d1ba1e40745200a
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-11-22 22:19:45 +0200

    Add files via upload

---
+++ SPRING DATA/JSON Processing/Exercise/JSON-Advanced/main/java/JsonAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsonadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonAdvancedApplication.class, args);
+    }
+
+}
