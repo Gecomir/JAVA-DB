043727162a9dd7c8ce77afc457dc1688018734a4
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-11-22 22:12:44 +0200

    Add files via upload

---
+++ SPRING DATA/JSON Processing/Exercise/JSON-Advanced/main/java/domain/dtos/categories/CategoryProductSummaryDto.java
@@ -0,0 +1,26 @@
+package com.example.jsonadvanced.domain.dtos.categories;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryProductSummaryDto {
+
+    @SerializedName("name")
+    private String category;
+
+    private Long productsCount;
+
+    private Double averagePrice;
+
+    private BigDecimal totalRevenue;
+
+}
