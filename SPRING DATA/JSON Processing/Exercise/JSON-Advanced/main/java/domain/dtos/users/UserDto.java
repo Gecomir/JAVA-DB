5aa23d018e9211d302d7c49aa38a6df6f4e2148d
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-11-22 22:15:16 +0200

    Add files via upload

---
+++ SPRING DATA/JSON Processing/Exercise/JSON-Advanced/main/java/domain/dtos/users/UserDto.java
@@ -0,0 +1,38 @@
+package com.example.jsonadvanced.domain.dtos.users;
+
+import com.example.jsonadvanced.domain.dtos.products.ProductDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+import static com.example.jsonadvanced.domain.dtos.products.ProductDto.toProductsSoldWithCountDto;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private String firstName;
+
+    private String lastName;
+
+    private Integer age;
+
+    private Set<ProductDto> sellingProducts;
+
+    private Set<ProductDto> boughtProducts;
+
+    private Set<UserDto> friends;
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public UserWithProductsDto toUserWithProductsDto() {
+        return new UserWithProductsDto(firstName, lastName, age, toProductsSoldWithCountDto(sellingProducts));
+    }
+}
